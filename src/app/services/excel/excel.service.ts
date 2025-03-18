c4f3acecaca4ae146cd3921c685256fb4bb35d49
nicolas.hoffmann <hoffmannni92690@th-nuernberg.de>
2025-03-18 16:37:28 +0100

    adjusted excel download

--- src/app/services/excel/excel.service.ts
+++ src/app/services/excel/excel.service.ts
@@ -181,7 +181,7 @@ export class ExcelService {
       const base64Data = btoa(binary);
       const dataUri = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,' + base64Data;
 
-      if (isPlatform('desktop') || isPlatform('mobileweb')) {
+      if (isPlatform('desktop') || isPlatform('mobileweb') || isPlatform('pwa')) {
         const anchor = document.createElement('a');
         anchor.href = dataUri;
         anchor.download = fileName;
